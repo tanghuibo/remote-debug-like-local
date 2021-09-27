97973cd8c1088d90e5747730374abaec3f61d354
GitHub Action <action@github.com>
2021-09-27 12:28:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/tanghuibo/remotedebuglikelocal/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/tanghuibo/remotedebuglikelocal/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.tanghuibo.remotedebuglikelocal.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.tanghuibo.remotedebuglikelocal.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/tanghuibo/remotedebuglikelocal/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.tanghuibo.remotedebuglikelocal.services
+
+import com.github.tanghuibo.remotedebuglikelocal.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
